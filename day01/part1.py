832f3e0c6e8ef6213e3877aed1f7656a1c2c1131
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2023-12-01 10:32:38 +0100

    day01

---
+++ day01/part1.py
@@ -0,0 +1,53 @@
+import argparse
+import os.path
+import re
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+'''
+EXPECTED = 142
+
+
+def compute(s: str) -> int:
+    total = 0
+    lines = s.splitlines()
+    for line in lines:
+        nums = re.findall(r'\d', line)
+        total += int(nums[0] + nums[-1])
+    return total
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
---
+++ day01/part2.py
@@ -0,0 +1,68 @@
+import argparse
+import os.path
+import re
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+# NOTE: paste test text here
+INPUT_S = '''\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+'''
+EXPECTED = 281
+
+words = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+w_to_n = {w: str(n) for n, w in enumerate(words, start=1)}
+
+pat = re.compile(fr'(?=(\d|{"|".join(words)}))')
+
+
+def compute(s: str) -> int:
+    total = 0
+    for line in s.splitlines():
+        # search
+        found = pat.findall(line)
+        first, last = found[0], found[-1]
+
+        # transform
+        first = w_to_n.get(first, first)
+        last = w_to_n.get(last, last)
+
+        # add
+        total += int(first + last)
+    return total
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
